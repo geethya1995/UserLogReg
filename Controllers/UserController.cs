bec889718c2cd97cde6720e73065a0aca32609d9
Geethya <geethya1995@gmail.com>
2020-12-14 23:59:19 +0530

    Add Registration Action

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -9,6 +9,23 @@ namespace TestProj.Controllers
     public class UserController : Controller
     {
         // Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();  
+        }
+
+        // Registration POST Action
+
+        // Verify E-mail (Verifying the Account/User)
+
+        // Verify E-mail link
+
+        // Login
+
+        // Login POST
+
+        // Logout
 
     }
 }
