0f856d1ca939df2bc1a2feb7be54e3a55fabe6a5
Geethya <geethya1995@gmail.com>
2020-12-15 10:16:16 +0530

    Registration - POST  (action stub created and IsEmailVerified , ActivationCode fields are excluded)

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestProj.Models;
 
 namespace TestProj.Controllers
 {
     public class UserController : Controller
     {
-        // Registration Action
+        // Registration Action (Render the form)
         [HttpGet]
         public ActionResult Registration()
         {
             return View();  
         }
 
-        // Registration POST Action
+        // Registration POST Action (Submit data to DB from the form)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified , ActivationCode")] User user)
+        {
+            // Submit and save the form data to User table (Logic)
+            return View(User);
+        }
 
         // Verify E-mail (Account validation)
 
