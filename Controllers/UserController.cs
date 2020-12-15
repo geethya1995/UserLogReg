2b2c8ede8c6b13b569dee4233fdd9b044c4dd909
Geethya <geethya1995@gmail.com>
2020-12-15 15:53:21 +0530

    Login POST action and Logout added

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using TestProj.Models;
 using System.Net.Mail;
 using System.Net;
+using System.Web.Security;
 
 namespace TestProj.Controllers
 {
@@ -116,8 +117,65 @@ namespace TestProj.Controllers
         }
 
         // Login POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login login, string ReturnUrl = "")
+        {
+            string message = "";
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.Email == login.Email).FirstOrDefault();
+                if (v != null)
+                {
+                    if (!v.IsEmailVerified)
+                    {
+                        ViewBag.Message = "Please verify your email first";
+                        return View();
+                    }
+                    if (string.Compare(Encryptor.Hash(login.Password), v.Password) == 0)
+                    {
+                        int timeout = login.RememberMe ? 525600 : 20; // 525600 min = 1 year
+                        var ticket = new FormsAuthenticationTicket(login.Email, login.RememberMe, timeout);
+                        string encrypted = FormsAuthentication.Encrypt(ticket);
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                        cookie.Expires = DateTime.Now.AddMinutes(timeout);
+                        cookie.HttpOnly = true;
+                        Response.Cookies.Add(cookie);
+
+
+                        if (Url.IsLocalUrl(ReturnUrl))
+                        {
+                            return Redirect(ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        message = "Invalid credential provided";
+                    }
+                }
+                else
+                {
+                    message = "Invalid credential provided";
+                }
+            }
+            ViewBag.Message = message;
+            return View();
+        }
+
 
         // Logout
+        [Authorize]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "User");
+        }
+
 
         [NonAction]
         public bool IsEmailExist(string emailID)
